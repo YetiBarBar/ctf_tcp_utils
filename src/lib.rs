bf985d72c5e419eb37c86309db10a17190ad8b8d
YetiBarBar <yetibarbar@gmail.com>
2024-02-25 22:25:34 +0100

    Fix: Fix run with timeout + remove unusable set_timeout function

--- src/lib.rs
+++ src/lib.rs
@@ -34,6 +34,27 @@ impl TcpHandler {
         Ok(Self { stream })
     }
 
+    /// Create a new `TcpHandler` wit a default 1s read timeout.
+    ///
+    /// # Errors
+    ///
+    /// May fail if server:port is unavaible or read timeout cannot be set.
+    pub fn new_with_timeout(
+        server_url: &str,
+        port: u16,
+        timeout: u64,
+    ) -> Result<Self, CtfTcpHandlerError> {
+        let stream = {
+            let connection_uri = format!("{server_url}:{port}");
+            TcpStream::connect(connection_uri).map_err(|_| CtfTcpHandlerError::ConnectionError)
+        }?;
+        stream
+            .set_read_timeout(Some(std::time::Duration::from_millis(timeout)))
+            .map_err(|_| CtfTcpHandlerError::ConfigurationError)?;
+
+        Ok(Self { stream })
+    }
+
     /// Read TCP stream until read timeout is reached. Always produces a String using UTF-8 lossy conversion.
     pub fn read_to_string(&mut self) -> String {
         let mut res = String::new();
@@ -54,21 +75,6 @@ impl TcpHandler {
     pub fn write_answer(&mut self, answer: &str) {
         let data = format!("{answer}\n");
         let _size = self.stream.write(data.as_bytes());
-    }
-
-    /// Set read timeout.
-    ///
-    /// timeout are milliseconds, given with a `u64`
-    ///
-    /// # Errors
-    ///
-    /// Fail if read timeout cannot be set.
-    pub fn set_timeout(&mut self, timeout: u64) -> Result<(), CtfTcpHandlerError> {
-        self.stream
-            .set_read_timeout(Some(std::time::Duration::from_millis(timeout)))
-            .map_err(|_| CtfTcpHandlerError::ConfigurationError)?;
-
-        Ok(())
     }
 }
 
@@ -98,7 +104,6 @@ pub fn run_function_loop(
 }
 
 /// Connect to a CTF TCP server and process the same function in loop.
-/// This version allow to set a timeout
 ///
 /// # Errors
 ///
@@ -109,12 +114,10 @@ pub fn run_function_loop_with_timeout(
     timeout: u64,
     function: impl Fn(&str) -> Option<String>,
 ) -> Result<String, CtfTcpHandlerError> {
-    let mut tcp_handle =
-        TcpHandler::new(url, port).map_err(|_| CtfTcpHandlerError::ConnectionError)?;
-    tcp_handle.set_timeout(timeout)?;
+    let mut tcp_handle = TcpHandler::new_with_timeout(url, port, timeout)
+        .map_err(|_| CtfTcpHandlerError::ConnectionError)?;
     loop {
         let input = tcp_handle.read_to_string();
-
         println!("{input}");
         if let Some(answer) = function(&input) {
             println!("{answer}");
